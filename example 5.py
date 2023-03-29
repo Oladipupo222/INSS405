359ad714cab1ccd16bb9e7a15f20261a8c181f63
Oladipupo222 <oladipupot0601@students.bowiestate.edu>
2023-03-29 19:33:01 -0400

    Lab4

---
+++ example 5.py
@@ -0,0 +1,70 @@
+print('0')
+print('1')
+print('2')
+print('3')
+print('4')
+for i in range(5):
+    print(i)
+
+for i in range(100):
+    if (i%2==0):
+        print(i)
+
+
+# for i in range(5):
+#     number1=input('enter a number')
+# #     print(number)
+
+for x in range(100):
+    if (x%2!=0):
+        print(x)
+
+
+for x in range(1,10):
+    print(x)
+
+for x in range(5,100):
+    print(x)
+
+for x in range(5,50,5):
+    print (x)
+
+for x in range(50):
+    print(x)
+
+# for x in range (10):
+#     print(rand(1,100))
+
+
+# import random
+# for x in range():
+#  print(random.randit(500,600))
+
+
+
+import random
+for x in range(30):
+    print(random.randint(1,500))
+
+
+# import random as rand
+# sum+0.00
+# for i in range(30):
+#     print(i)
+#     sum=sum+rand.randit(1,500)
+# print("sum=,sum")
+
+import random as rand
+sum==0.00
+for i in range(5):
+    print("1=",i)
+    sum=sum+print(rand.randit(i,5))
+    print("sum=",sum)
+print("final sum",sum)
+average=
+    print(average.average(1,5))
+
+
+
+
+
